d69be94d89b70470f25efc9e08f5b0d4726f0cdd
vitoriafatima <vitoriatorralvo.fatima@gmail.com>
2021-09-18 19:47:29 -0300

    vitoriafatima

---
+++ 3.js
@@ -0,0 +1,4 @@
+for (var i =1; i<=10;i++){
+    var row ="8 * " + i +"=" + 8 * i;
+    console.log(row);
+}
---
+++ 4.js
@@ -0,0 +1,10 @@
+function ImprimirTabuada(n) {
+    for (var i = 1; i <= 10; i++){
+        var linha = n+ "*"+i+"="+n*i;
+        console.log(linha);
+    }
+}
+for (var i=1; i<= 10; i++) {
+    ImprimirTabuada(i);
+    console.log("");
+}
