1ca3361979698c2d158d9e8ae195b642b746769a
Vitoria Fatima <89617006+vitoriafatima@users.noreply.github.com>
2021-09-18 20:02:28 -0300

    Revert "vitoriafatima"

--- 3.js
+++
@@ -1,4 +0,0 @@
-for (var i =1; i<=10;i++){
-    var row ="8 * " + i +"=" + 8 * i;
-    console.log(row);
-}
--- 5.js
+++
@@ -1,7 +0,0 @@
-
-
-var soma = 0;
-for(var i =1;i <= 10; i++){
-    soma +=i;
-}
-console.log(soma);
